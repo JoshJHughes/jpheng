e6c50a0d85753c188c56fc40cdf2b9122fc45818
Josh Hughes <Josh Hughes>
2017-06-26 14:29:59 +0100

    Added various spring force generators and examples of their use

--- jpheng/force_generators.py
+++ jpheng/force_generators.py
@@ -25,8 +25,7 @@ class GravityGenerator(ForceGenerator):
         # objects with inv_mass = 0 are considered fixed in place
         if physics.inv_mass == 0:
             return
-        else:
-            physics.force_accum += self.g/physics.inv_mass
+        physics.force_accum += self.g/physics.inv_mass
 
 class ParticleSpring(ForceGenerator):
     """Generator for spring force, each end is attached to a physics 
@@ -37,8 +36,7 @@ class ParticleSpring(ForceGenerator):
         k: spring constant
         l0: natural length of the spring
         other_physics: physics component which is anchored at the other 
-        end of the
-            spring
+        end of the spring
     Methods:
         update_force: Update the force accumulator in the physics component 
             based on a time step of 'duration'.
@@ -50,7 +48,54 @@ class ParticleSpring(ForceGenerator):
     def update_force(self, physics, duration):
         if physics.inv_mass == 0:
             return
-        else:
-            d = physics.p - self.other_physics.p
-            f = -self.k*(np.abs(np.linalg.norm(d))-self.l0)*d/np.linalg.norm(d)
-            physics.force_accum += f
+        d = physics.p - self.other_physics.p
+        f = -self.k*(np.abs(np.linalg.norm(d))-self.l0)*d/np.linalg.norm(d)
+        physics.force_accum += f
+
+class AnchoredSpring(ForceGenerator):
+    """Generator for spring force, one end is attached to a physics
+    component, the other is attached to a fixed point in space.
+        Variables:
+            k: spring constant
+            l0: natural length of the spring
+            anchor: numpy array containing the anchor point of the spring
+        Methods:
+            update_force: Update the force accumulator in the physics component
+                based on a time step of 'duration'.
+        """
+    def __init__(self, anchor, k, l0):
+        self.anchor = np.array(anchor)
+        self.k = k
+        self.l0 = l0
+    def update_force(self, physics, duration):
+        if physics.inv_mass == 0:
+            return
+        d = physics.p - self.anchor
+        f = -self.k*(np.abs(np.linalg.norm(d))-self.l0)*d/np.linalg.norm(d)
+        physics.force_accum += f
+
+class AnchoredBungee(ForceGenerator):
+    """Generator for spring force, one end is attached to a physics
+    component, the other is attached to a fixed point in space.  Bungees
+    provide extensive spring force but no compressive spring force.
+        Variables:
+            k: spring constant
+            l0: natural length of the bungee
+            anchor: numpy array containing the anchor point of the bungee
+        Methods:
+            update_force: Update the force accumulator in the physics component
+                based on a time step of 'duration'.
+        """
+    def __init__(self, anchor, k, l0):
+        self.anchor = np.array(anchor)
+        self.k = k
+        self.l0 = l0
+    def update_force(self, physics, duration):
+        if physics.inv_mass == 0:
+            return
+        d = physics.p - self.anchor
+        if np.abs(np.linalg.norm(d)) <= self.l0:
+            return
+        f = -self.k*(np.abs(np.linalg.norm(d))-self.l0)*d/np.linalg.norm(d)
+        physics.force_accum += f
+
--- scripts/springs_demo.py
+++ scripts/springs_demo.py
@@ -20,45 +20,80 @@ if __name__ == '__main__':
                             caption="jpheng Demo", resizable=True)
     window.set_exclusive_mouse(True)
 
+    # Particle Spring
     # create particle1
-    p1 = [40, -20, 10]
-    v1 = [0, 10, 0]
+    p1 = [40, -40, 10]
+    v1 = [0, 0, 0]
     a1 = [0, 0, 0]
     inv_mass = 1/5
     r = 1
-    particle1 = entities.Particle(p1, v1, a1, inv_mass, r, color=(187, 86,
-                                                                  103))
+    particle1 = entities.Particle(p1, v1, a1, inv_mass, r, color=(97, 86, 103))
     # create particle2
-    p2 = [-40, 20, 40]
-    v2 = [0, 10, 0]
+    p2 = [-40, -40, 10]
+    v2 = [0, 0, 0]
     a2 = [0, 0, 0]
     inv_mass = 1 / 5
     r = 1
-    particle2 = entities.Particle(p2, v2, a2, inv_mass, r, color=(187, 86,
-                                                                  103))
-
+    particle2 = entities.Particle(p2, v2, a2, inv_mass, r, color=(97, 86, 103))
     # create marker between particles
-    pm = [0,0,20]
-    vm = [0,0,0]
-    am = [0,0,0]
-    marker = entities.Particle(pm, vm, am, inv_mass, r, color=(0,0,0))
-
-    # remove gravity from particles
-    marker.physics.g = np.zeros(3)
+    pm1 = [0,-40,10]
+    vm1 = [0,0,0]
+    am1 = [0,0,0]
+    marker1 = entities.Particle(pm1, vm1, am1, inv_mass, r, color=(0,0,0))
 
     # add spring force to particle1
-    k = 10
-    l0 = 70
-    spring1 = force.ParticleSpring(particle2.physics, k, l0)
-    spring2 = force.ParticleSpring(particle1.physics, k, l0)
+    k1 = 10
+    l01 = 70
+    spring1 = force.ParticleSpring(particle2.physics, k1, l01)
+    spring2 = force.ParticleSpring(particle1.physics, k1, l01)
     particle1.physics.add_generator(spring1)
     particle2.physics.add_generator(spring2)
 
+    # Anchored Spring
+    p3 = [40, 40, 10]
+    v3 = [0,0,0]
+    a3 = [0,0,0]
+    particle3 = entities.Particle(p3, v3, a3, inv_mass, r, color=(97, 36, 83))
+    pm2 = [0, 40, 10]
+    vm2 = [0, 0, 0]
+    am2 = [0, 0, 0]
+    marker2 = entities.Particle(pm2, vm2, am2, inv_mass, r, color=(0,0,0))
+    k2 = 10
+    l02 = 30
+    anchor = pm2
+    anchored_spring = force.AnchoredSpring(anchor, k2, l02)
+    particle3.physics.add_generator(anchored_spring)
 
+    # Anchored Bungee
+    p4 = [40, 0, 40]
+    v4 = [0,0,0]
+    a4 = [0,0,0]
+    particle4 = entities.Particle(p4, v4, a4, inv_mass, r, color=(97,36,83))
+    pm3 = [40, 0, 50]
+    vm3 = [0,0,0]
+    am3 = [0,0,0]
+    marker3 = entities.Particle(pm3, vm3, am3, inv_mass, r, color=(0,0,0))
+    k3 = 20
+    l03 = 20
+    anchor = pm3
+    anchored_bungee = force.AnchoredBungee(anchor, k3, l03)
+    particle4.physics.add_generator(anchored_bungee)
+
+    # remove gravity from particles and markers
+    particle1.physics.g = np.zeros(3)
+    particle2.physics.g = np.zeros(3)
+    particle3.physics.g = np.zeros(3)
+    marker1.physics.g = np.zeros(3)
+    marker2.physics.g = np.zeros(3)
+    marker3.physics.g = np.zeros(3)
     # add particles to window
     window.add_entity(particle1)
     window.add_entity(particle2)
-    window.add_entity(marker)
+    window.add_entity(particle3)
+    window.add_entity(particle4)
+    window.add_entity(marker1)
+    window.add_entity(marker2)
+    window.add_entity(marker3)
 
     # enter main program loop
     pyglet.app.run()
