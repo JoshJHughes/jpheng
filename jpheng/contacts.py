ec8ef694ae1612416fbd7eeaad20ef0b04d81958
Josh Hughes <Josh Hughes>
2017-06-28 17:02:41 +0100

    Added EntityContact class to handle contact resolution.
    Added collision_demo script to test collision resolution

---
+++ jpheng/contacts.py
@@ -0,0 +1,100 @@
+import numpy as np
+
+class EntityContact:
+    """An EntityContact represents two entities in contact.  Resolving a
+    contact removes interpenetration and applies sufficient impulse to keep
+    them apart.  Colliding bodies may also rebound.
+
+    This class's methods should not be called, to resolve a set of contacts
+    use the EntityContactResolver class.
+    Variables:
+        entities: List containing the entities involved in the contact.
+            If the second element is None then the collision is with a
+            non-particle object, e.g. the scenery.
+        restitution: Coefficient of restitution for the contact.  v_after =
+            -restitution*v_before
+        normal: The contact normal, 3D numpy array [x,y,z].  Given from the
+            perspective of self.entities[0] (subscript a),  n = p_a - p_b
+            for particles.  Must be normalised.
+        penetration: The depth of penetration at the contact in the
+            direction of the contact normal
+    Methods:
+        resolve: Resolves contact for velocity and interpenetration
+        calc_separation_velocity: Returns the separation velocity of the
+            entities in contact
+        resolve_velocity: Resolves the final velocity of each entity.
+            Energy loss is described by the coefficient of restitution.
+            Substitute the separation velocity into the equation for
+            conservation of momentum to obtain:
+            v_a' = v_a - (m_b/(m_a + m_b))*(1+c)*v_s*n
+            v_b' = v_b + (m_a/(m_a + m_b))*(1+c)*v_s*n
+            where c is the coefficient of restitution, v_s is the separation
+            velocity, and n is the contact normal.
+        resolve_interpenetration: Moves each entity apart along the contact
+            normal so that they no longer penetrate each other.  Movement is
+            done in proportion to the inverse masses of the entities.
+            p_a' = p_a + (m_b/(m_a+m_b))*d*n
+            p_b' = p_b - (m_a/(m_a+m_b))*d*n
+            where d is the penetration depth and n is the contact normal
+    """
+    def __init__(self, entities, restitution, normal, penetration):
+        self.entities = entities
+        self.restitution = restitution
+        self.normal = normal
+        self.penetration = penetration
+
+    def resolve(self, duration):
+        """Resolves this contact, for both velocity and interpenetration."""
+        self.resolve_velocity(duration)
+        self.resolve_interpenetration(duration)
+
+    def calc_separation_velocity(self):
+        """Calculates the separation velocity at this contact."""
+        v_rel = self.entities[0].physics.v
+        if self.entities[1] is not None:
+            v_rel = v_rel - self.entities[1].physics.v
+        return np.dot(v_rel,self.normal)
+
+    def resolve_velocity(self, duration):
+        """Handles the impulse calculations for this collision."""
+        v_sep = self.calc_separation_velocity()
+        # if v_sep >=0 then contact is either separating or stationary and
+        #  no impulse is required
+        if v_sep >= 0:
+            return
+
+        # get total inverse mass
+        total_inv_mass = self.entities[0].physics.inv_mass
+        if self.entities[1] is not None:
+            total_inv_mass += self.entities[1].physics.inv_mass
+        # if all entities have infinite mass then there is no effect
+        if total_inv_mass == 0: return
+
+        # calculate changes in speed for each entity and apply them in the
+        # direction of the contact normal
+        dv_a = -self.entities[0].physics.inv_mass*(1 +
+                self.restitution)*v_sep/total_inv_mass
+        self.entities[0].physics.v += dv_a*self.normal
+        if self.entities[1] is not None:
+            dv_b = self.entities[1].physics.inv_mass*(1 +
+                    self.restitution)*v_sep/total_inv_mass
+            self.entities[1].physics.v += dv_b*self.normal
+
+    def resolve_interpenetration(self, duration):
+        # if there is no penetration then return
+        if self.penetration <= 0: return
+
+        # get total inverse mass
+        total_inv_mass = self.entities[0].physics.inv_mass
+        if self.entities[1] is not None:
+            total_inv_mass += self.entities[1].physics.inv_mass
+        # if all entities have infinite mass then there is no effect
+        if total_inv_mass == 0: return
+
+        self.entities[0].physics.p += \
+            self.entities[0].physics.inv_mass*self.penetration*self.normal\
+            /total_inv_mass
+        if self.entities[1].physics.p is not None:
+            self.entities[1].physics.p -= \
+                self.entities[1].physics.inv_mass*self.penetration*self.normal\
+                /total_inv_mass
--- scripts/collision_demo.py
+++ scripts/collision_demo.py
@@ -6,6 +6,7 @@ import jpheng.force_generators as force
 import numpy as np
 import jpheng.physics as phy
 import jpheng.graphics as gra
+import jpheng.contacts as contacts
 
 # This demo is intended to showcase a single particle moving around the
 # 'EmptyMap' level.  Press escape to exit the program.
@@ -13,21 +14,56 @@ import jpheng.graphics as gra
 
 if __name__ == '__main__':
     # create level map
-    level_map = maps.EmptyMap()
+    level_map = maps.EmptyMap(x_lim = 60, y_lim = 60)
 
     # create window
-    window = windows.Window(level_map, width = 800, height = 600,
-                            caption="jpheng Demo", resizable=True)
+    window = windows.Window(level_map, caption="jpheng Demo", resizable=True,
+                            fullscreen=True)
     window.set_exclusive_mouse(True)
 
-    # create particle
-    p = [0, 0, 20]
-    v = [70, 50, 0]
-    a = [0, 0, 0]
+    # create particles
+    p1 = [45, 2.5, 20]
+    v1 = [-120, 0, 0]
+    a1 = [0, 0, 0]
     inv_mass = 1/5
-    r = 1
-    particle = entities.Particle(p, v, a, inv_mass, r, color=(187, 86, 103))
-    window.add_entity(particle)
+    r = 10
+    particle1 = entities.Particle(p1, v1, a1, inv_mass, r, color=(187, 86,
+                                                                  103))
+
+    p2 = [-45, -3, 20]
+    v2 = [140, 0, 0]
+    a2 = [0, 0, 0]
+    inv_mass = 1/10
+    r = 10
+    particle2 = entities.Particle(p2, v2, a2, inv_mass, r, color=(36, 96,
+                                                                  201))
+
+    # add particles to window
+    window.add_entity(particle1)
+    window.add_entity(particle2)
+
+    # simple collision detection, to give a useful demo until prper routines
+    #  are implemented
+    def detect_collision(duration, entity1, entity2):
+        p1 = entity1.physics.p
+        p2 = entity2.physics.p
+
+        r1 = entity1.graphics.r
+        r2 = entity2.graphics.r
+
+        p_sep = p1 - p2
+
+        if np.abs(np.linalg.norm(p_sep)) < (r1 + r2):
+            entities = [entity1, entity2]
+            restitution = 1
+            normal = p_sep/np.linalg.norm(p_sep)
+            penetration = r1 + r2 - np.abs(np.linalg.norm(p_sep))
+            contact = contacts.EntityContact(entities, restitution, normal,
+                                             penetration)
+            contact.resolve(duration)
+
+    pyglet.clock.schedule_interval(detect_collision, 1/120,
+                                   particle1, particle2)
 
     # enter main program loop
     pyglet.app.run()
