722ad02d0cad9000f50a19e6f5f2f0252a71750f
Josh Hughes <Josh Hughes>
2017-06-28 17:02:25 +0100

    Added EntityContact class to handle contact resolution.
    Added collision_demo script to test collision resolution

---
+++ scripts/collision_demo.py
@@ -0,0 +1,33 @@
+import pyglet
+import jpheng.entities as entities
+import jpheng.window as windows
+import jpheng.maps as maps
+import jpheng.force_generators as force
+import numpy as np
+import jpheng.physics as phy
+import jpheng.graphics as gra
+
+# This demo is intended to showcase a single particle moving around the
+# 'EmptyMap' level.  Press escape to exit the program.
+
+
+if __name__ == '__main__':
+    # create level map
+    level_map = maps.EmptyMap()
+
+    # create window
+    window = windows.Window(level_map, width = 800, height = 600,
+                            caption="jpheng Demo", resizable=True)
+    window.set_exclusive_mouse(True)
+
+    # create particle
+    p = [0, 0, 20]
+    v = [70, 50, 0]
+    a = [0, 0, 0]
+    inv_mass = 1/5
+    r = 1
+    particle = entities.Particle(p, v, a, inv_mass, r, color=(187, 86, 103))
+    window.add_entity(particle)
+
+    # enter main program loop
+    pyglet.app.run()
